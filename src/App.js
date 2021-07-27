97622859ad3bb6ceae96e53a45b6c852d44d00c3
huseyinturkmenoglu <turkmenogluhuseyin@gmail.com>
2021-07-27 06:59:25 +0300

    Added project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Create from './components/Create';
+import Read from './components/Read';
+import Update from './components/Update';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="main">
+        <h2 className="main-header">React Crud Operations</h2>
+        <div>
+          <Route exact path="/create" component={Create} />
+        </div>
+        <div style={{ marginTop: 20 }}>
+          <Route exact path='/read' component={Read} />
+        </div>
+
+        <Route path='/update' component={Update} />
+        
+      </div>
+    </Router>
   );
 }
 
